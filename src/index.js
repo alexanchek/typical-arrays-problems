72b9d9c1f9a078d2471d87677ad0ef82b1d4b443
Alexander <alexanchek@sashkin-blog.ru>
2021-02-10 22:21:57 +0300

    fix: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    let min = 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (array ===undefined || array.length===0 ) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+    }
+    return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array ===undefined || array.length===0 ) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+    }
+    return max;
+};
+
+exports.avg = function avg(array) {
+    let avg = 0;
+    let sum = 0;
+
+    if (array ===undefined || array.length===0 ){
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+    }
+    avg = parseFloat(sum/array.length)
+
+    return avg;
+};
